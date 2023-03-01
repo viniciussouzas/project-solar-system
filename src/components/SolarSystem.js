eb93b61b387a2644035876391aa54c2ed153cdc6
Vinícius Souza <tradutorvinicius@gmail.com>
2023-03-01 15:33:43 -0300

    Resolve requisito 2

---
+++ src/components/SolarSystem.js
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+class SolarSystem extends Component {
+  render() {
+    return (
+      <div data-testid="solar-system">
+        {}
+      </div>
+    );
+  }
+}
+
+export default SolarSystem;
