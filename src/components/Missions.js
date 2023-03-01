e48c60028549635423ed50acf243f82275606086
Vinícius Souza <tradutorvinicius@gmail.com>
2023-03-01 16:19:21 -0300

    Resolve requisito 8

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,10 +1,11 @@
 import { Component } from 'react';
+import Title from './Title';
 
 class Missions extends Component {
   render() {
     return (
       <div data-testid="missions">
-        {}
+        <Title headline="Missões" />
       </div>
     );
   }
