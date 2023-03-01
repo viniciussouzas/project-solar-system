bc8ff09768d8850d175ba3f7cd4ba201983ddb79
Vinícius Souza <tradutorvinicius@gmail.com>
2023-03-01 15:29:48 -0300

    Resolve requisito 1

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <h1>Sistema Solar</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
